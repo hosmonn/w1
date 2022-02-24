be406c9c17632d47db24de025ad2e7c5c858999c
hosmonn <hosmon20@gmail.com>
2022-02-24 20:36:46 +0100

    1

---
+++ assets/scripts/config.js
@@ -0,0 +1,17 @@
+DataConnectors = [
+];
+
+var PageFabricConfig = {
+disqusId : "",
+googleAnalyticsId : "",
+globalSearchMode : "Webservice",
+globalSearchWebServiceURL : "",
+globalSearchLanguageFiles:[
+{
+ lang: "en", 
+ url: "/en.json"
+},{
+ lang: "es", 
+ url: "/es.json"
+}]
+};
